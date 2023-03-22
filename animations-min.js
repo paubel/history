27e6f6f3eabec57f0b359f42f673fd63ab8e03e9
paubel <paul.belfrage@gmail.com>
2023-03-22 08:51:23 +0100

    ani

---
+++ animations-min.js
@@ -0,0 +1,268 @@
+jQuery(document).ready(function ($) {
+  $(".arrow").each(function () {
+    $(this).height($(this).width());
+  }),
+    $(".dot").each(function () {
+      $(this).height($(this).width());
+    });
+  setTimeout(function () {
+    $(".animation-retail-connect .cls-23.one").addClass("lit");
+  }, 1600),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.five").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.six").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.seven").addClass("lit");
+    }, 800),
+    setTimeout(function () {
+      $(".animation-retail-connect .cls-23.eight").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-retail-connect").addClass("move-dots");
+    }, 2100),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.five").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.six").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.seven").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.eight").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.nine").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.ten").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.eleven").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.twelve").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.thirteen").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.fourteen").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.fifteen").addClass("lit");
+    }, 800),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.sixteen").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.seventeen").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.eighteen").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-connect-device .cls-12.nineteen").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-connect-device").addClass("move-dots");
+    }, 2100),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.five").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.six").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.seven").addClass("lit");
+    }, 800),
+    setTimeout(function () {
+      $(".animation-pay-connect .screen.eight").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-pay-connect").addClass("move-dots");
+    }, 2100),
+    setTimeout(function () {
+      $(".animation-vault-connect .screen.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-vault-connect .screen.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-vault-connect .screen.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-vault-connect .screen.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-vault-connect").addClass("move-dots");
+    }, 2100),
+    setTimeout(function () {
+      $(".animation-vault-connect .dot.two").addClass("lit");
+    }, 2e3),
+    setTimeout(function () {
+      $(".animation-vault-connect .dot.three").addClass("lit");
+    }, 3200),
+    setTimeout(function () {
+      $(".animation-vault-connect .dot.four").addClass("lit");
+    }, 4400),
+    setTimeout(function () {
+      $(".animation-vault-connect .dot.one").addClass("lit");
+    }, 5600),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.five").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.six").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.seven").addClass("lit");
+    }, 800),
+    setTimeout(function () {
+      $(".animation-zero-touch .cls-12.eight").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.five").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.six").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.seven").addClass("lit");
+    }, 800),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.eight").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.nine").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.ten").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-cube .cls-12.eleven").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-cube").addClass("move-dots");
+    }, 2100),
+    setTimeout(function () {
+      $(".animation-transport-connect .screen.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-transport-connect .screen.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-transport-connect .screen.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-transport-connect .screen.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-transport-connect").addClass("move-dots");
+    }, 2100),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.one").addClass("lit");
+    }, 1600),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.two").addClass("lit");
+    }, 600),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.three").addClass("lit");
+    }, 1400),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.four").addClass("lit");
+    }, 200),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.five").addClass("lit");
+    }, 400),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.six").addClass("lit");
+    }, 1e3),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.seven").addClass("lit");
+    }, 800),
+    setTimeout(function () {
+      $(".animation-connect-platform .screen.eight").addClass("lit");
+    }, 1200),
+    setTimeout(function () {
+      $(".animation-connect-platform").addClass("move-dots");
+    }, 2100),
+    $(window)
+      .resize(function () {
+        $(".arrow").each(function () {
+          $(this).height($(this).width());
+        }),
+          $(".dot").each(function () {
+            $(this).height($(this).width());
+          });
+      })
+      .resize();
+  bodymovin.loadAnimation({
+    container: document.getElementById("bm"),
+    renderer: "svg",
+    loop: !0,
+    autoplay: !0,
+    path: "wp-content/themes/PayComplete/assets/animations/_Home/home-hero.json",
+  });
+});
